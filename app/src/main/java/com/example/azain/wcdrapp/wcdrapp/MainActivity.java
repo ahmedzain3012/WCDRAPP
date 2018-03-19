7efa8b286fa064f294c5183e04f4797d09359e0c
Ahmed Zain <arz3012@gmail.com>
2018-03-19 13:21:37 +0300

    hi2

--- app/src/main/java/com/example/azain/wcdrapp/wcdrapp/MainActivity.java
+++ app/src/main/java/com/example/azain/wcdrapp/wcdrapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.azain.wcdrapp.wcdrapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText nIdNumber = findViewById(R.id.nid_number);
+        final EditText salaryNo = findViewById(R.id.salary_no);
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                if (nIdNumber.getText().toString().equals("2238551234") && salaryNo.getText().toString().equals("3784")) {
+                    Toast.makeText(MainActivity.this,"Done " +nIdNumber.getText().toString() +" - "+ salaryNo.getText().toString(), Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this, CustomerActivity.class);
+                    startActivity(i);
+                }else{
+                    Toast.makeText(MainActivity.this, "error "+nIdNumber.getText().toString() +" - "+ salaryNo.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
     }
 }
